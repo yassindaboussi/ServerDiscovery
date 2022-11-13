872df7c8506a70b26d3979cffc5791bce9208379
Yassin Daboussi <yassin.daboussi@esprit.tn>
2022-11-13 18:12:54 +0100

    R

---
+++ server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+var bodyParser = require("body-parser");
+const socketIO = require("socket.io");
+var fs = require("fs");
+const path = require("path");
+//////////////////////////////////////////////////////////////////////////////
+const app = express();
+const PORT = process.env.PORT || 9090;
+//const databaseName = "DiscoveryBD";
+//////////////////////////////////////////////////////////////////////////////
+const dotenv = require("dotenv"); // Fichier .env (All My Secret Key)
+dotenv.config();
+//////////////////////////////////////////////////////////////////////////////
+mongoose.set("debug", true);
+mongoose.Promise = global.Promise;
+//////////////////////////////////////////////////////////////////////////////
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+//////////////////////////////////////////////////////////////////////////////
+//console.log(__dirname + "/uploads");
+mongoose
+  .connect(process.env.MONGO_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+");
+    console.log("MongoDB Database is connected!");
+    console.log("-+-+-+-+-+-+-+-+-+-+-+-+-+-++-+");
+  })
+  .catch((ex) => {
+    console.log(ex);
+    console.log("Unable to connect to database");
+  });
+//////////////////////////////////////////////////////////////////////////////
+app.use(express.json());
+app.use(morgan("dev")); // Utiliser morgan
+app.use(express.urlencoded({ extended: true })); // Pour analyser application/x-www-form-urlencoded
+//////////////////////////////////////////////////////////////////////////////
+////////////////////Routes
+app.use("/api/user", require("./routes/user.route"));
+/////////////////// Default Page
+
+/////////////////// http://localhost:9090/imaguser/image-1668355834015.jpg
+app.use("/imaguser", express.static(path.join(__dirname, "uploads", "users"))); //
+/////////////////////////////////////////////////////////////////////////
+const listingPath = path.join(__dirname, "uploads", "users");
+app.get("*", (req, res) => {
+  // Build the path of the file using the URL pathname of the request.
+  const filePath = path.join(listingPath, req.path);
+  // If the path does not exist, return a 404.
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).end();
+  }
+  // Check if the existing item is a directory or a file.
+  if (fs.statSync(filePath).isDirectory()) {
+    const filesInDir = fs.readdirSync(filePath);
+    // If the item is a directory: show all the items inside that directory.
+    return res.send(filesInDir);
+  } else {
+    const fileContent = fs.readFileSync(filePath, "utf8");
+    // If the item is a file: show the content of that file.
+    return res.send(fileContent);
+  }
+});
+//////////////////////////////////////////////////////////////////////////////
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
+});
