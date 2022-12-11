f7c45ff252e740490423ada46b1c8f88be067242
Yassin Daboussi <35609041+yassindaboussi@users.noreply.github.com>
2022-12-11 18:53:17 +0100

    Update postuser.route.js

--- routes/postuser.route.js
+++ routes/postuser.route.js
@@ -2,10 +2,46 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/postuser.controller");
 const RequireLogin = require("../middleware/RequireLogin");
+const CheckFolderUpload = require("../middleware/CheckFolderUpload");
+
+//
+var multer, storage, path, crypto;
+multer = require("multer");
+path = require("path");
+crypto = require("crypto");
+var fs = require("fs");
+//
+//configure multer
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/posts");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + "-" + Date.now() + ".jpg");
+  },
+});
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 9000000, // Max 9 MB
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png|JPG|JPEG|PNG)$/)) {
+      return cb(new Error("Please upload a valid image file"));
+    }
+    cb(undefined, true);
+  },
+});
 
 ///////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
 router.get("/all", postController.index); // Show All Users
-router.post("/AddPostUser", postController.AddPostUser);
 router.post("/GetAllMyPost", postController.GetAllMyPost);
+router.post(
+  "/AddPostUser",
+  CheckFolderUpload,
+  upload.single("image"),
+  postController.AddPostUser
+); // Upload Avatar
 
 module.exports = router;
