0311aaac6492720c8737044654d4c74e92ab41d7
Yassin Daboussi <35609041+yassindaboussi@users.noreply.github.com>
2022-12-22 14:56:19 +0100

    Update user.model.js

--- models/user.model.js
+++ models/user.model.js
@@ -14,6 +14,10 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  role: {
+    type: String,
+    required: true,
+  },
   avatar: {
     type: String,
     required: false,
