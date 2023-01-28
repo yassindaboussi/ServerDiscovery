3a15f5917fde7ebbb82be93cc656290d7725479a
Yassin Daboussi <35609041+yassindaboussi@users.noreply.github.com>
2023-01-28 14:22:21 +0100

    Verified NotYet (huawei Deploy)

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -103,7 +103,7 @@ const signup = async (req, res, next) => {
       bio: RandomBio,
       codeVerif: "empty",
       codeForget: "empty",
-      verified: "NotYet",
+      verified: "YES",//NotYet
       role: "User",
     });
     user
